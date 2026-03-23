b7b197cdd6d9254a0a074bb814cbb2d43fa225a7
Md. Habibul Hasan <hasanthp@gmail.com>
2026-03-23 10:35:03 +0600

    Deployment bug fixes

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,8 @@
-// components/theme-provider.tsx
 "use client";
+// components/theme-provider.tsx
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist/types";
+
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
